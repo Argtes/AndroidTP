a11043a6e27a6234f651a8e8c114fde2d4087a76
Marc-olivier PRATS <marc-olivier.prats@etudiant.univ-lille1.fr>
2015-10-12 22:18:30 +0200

    modif

--- app/src/main/java/com/example/marc_olivier/androidtp/Modele.java
+++ app/src/main/java/com/example/marc_olivier/androidtp/Modele.java
@@ -1,44 +1,48 @@
 package pratsm.iut.myapplication2.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class Modele {
- /*       public Modele() {
-                tablettes = getResources().getStringArray(R.array.tablets);
-/* Ci-dessous le code permettant d'initialiser le tableau tablettes à partir du nombre de tablettes et du préfixe supposés
-existant dans les références. */
+import com.example.marc_olivier.androidtp.R;
 
-// SharedPreferences mySP = getSharedPreferences(MY_PREFS, Activity.MODE_PRIVATE) ;
-// int nbTablettes = mySP.getInt(NB_TABS, NB_TABS_DEF) ;
-// String radTab = mySP.getString(RAD_TABS, RAD_TABS_DEF) ;
-// tablettes = new String[nbTablettes] ;
-// for (int i = 1 ; i <= nbTablettes ; i++) {
-// tablettes[i] = radTab + i;
-// }
+public class Modele {
+       public Modele() {
+                tablettes = getResources().getStringArray(R.array.tablets);/*
+ Ci-dessous le code permettant d'initialiser le tableau tablettes ï¿½ partir du nombre de tablettes et du prï¿½fixe supposï¿½s
+existant dans les rï¿½fï¿½rences. */
+
+ SharedPreferences mySP = getSharedPreferences(MY_PREFS, Activity.MODE_PRIVATE) ;
+ int nbTablettes = mySP.getInt(NB_TABS, NB_TABS_DEF) ;
+ String radTab = mySP.getString(RAD_TABS, RAD_TABS_DEF) ;
+ tablettes = new String[nbTablettes] ;
+ for (int i = 1 ; i <= nbTablettes ; i++) {
+ tablettes[i] = radTab + i;
+ }
+
 // RAZ tableau students.
- /*               students = new int[tablettes.length];
+               students = new int[tablettes.length];
                 for (int i = 0; i < students.length; i++) {
                         students[i] = 0;
                 }
         }
 
-        /* Reconstruction à partir d'un bundle */
-  /*      public Modele(Bundle inState) {
+  /* Reconstruction ï¿½ partir d'un bundle */
+        public Modele(Bundle inState) {
                 tablettes = getResources().getStringArray(R.array.tablets);
                 students = inState.getIntArray("students");
         }
 
         /* Sauvegarde vers un Bundle */
-  /*      public void save(Bundle outState) {
+        public void save(Bundle outState) {
                 outState.putIntArray("students", students);
         }
-/* Fonction d'accès aux données */
+/* Fonction d'accï¿½s aux donnï¿½es */
 
         /**
          * GetCount: retourne le nombre de tablettes.
          */
-    /*    public int getCount() {
+        public int getCount() {
                 return tablettes.length;
         }
-*/
+
 }
