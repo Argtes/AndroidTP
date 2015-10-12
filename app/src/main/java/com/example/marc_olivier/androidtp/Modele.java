c7574c98fbe3b5f60fe11f1272dd803b83cd7abc
Argtes <stfu.gtfo@laposte.net>
2015-10-12 22:10:49 +0200

    modele.java fait

---
+++ app/src/main/java/com/example/marc_olivier/androidtp/Modele.java
@@ -0,0 +1,44 @@
+package pratsm.iut.myapplication2.app;
+
+import android.os.Bundle;
+
+public class Modele {
+ /*       public Modele() {
+                tablettes = getResources().getStringArray(R.array.tablets);
+/* Ci-dessous le code permettant d'initialiser le tableau tablettes à partir du nombre de tablettes et du préfixe supposés
+existant dans les références. */
+
+// SharedPreferences mySP = getSharedPreferences(MY_PREFS, Activity.MODE_PRIVATE) ;
+// int nbTablettes = mySP.getInt(NB_TABS, NB_TABS_DEF) ;
+// String radTab = mySP.getString(RAD_TABS, RAD_TABS_DEF) ;
+// tablettes = new String[nbTablettes] ;
+// for (int i = 1 ; i <= nbTablettes ; i++) {
+// tablettes[i] = radTab + i;
+// }
+// RAZ tableau students.
+ /*               students = new int[tablettes.length];
+                for (int i = 0; i < students.length; i++) {
+                        students[i] = 0;
+                }
+        }
+
+        /* Reconstruction à partir d'un bundle */
+  /*      public Modele(Bundle inState) {
+                tablettes = getResources().getStringArray(R.array.tablets);
+                students = inState.getIntArray("students");
+        }
+
+        /* Sauvegarde vers un Bundle */
+  /*      public void save(Bundle outState) {
+                outState.putIntArray("students", students);
+        }
+/* Fonction d'accès aux données */
+
+        /**
+         * GetCount: retourne le nombre de tablettes.
+         */
+    /*    public int getCount() {
+                return tablettes.length;
+        }
+*/
+}
